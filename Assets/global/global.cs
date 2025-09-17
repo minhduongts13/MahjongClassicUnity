9bebaa816cfa76f36aa47c735812d7581baba46b
minhduongts13 <quangminh4141@gmail.com>
2025-09-17 10:23:15 +0700

    adjusted assets loader, turned image to single, added global enum

--- Assets/global/global.cs
+++ Assets/global/global.cs
@@ -2,3 +2,17 @@ public enum THEME
 {
 
 }
+
+public enum THEME_STYLE
+{
+    Green,
+    Orange,
+    Violet,
+    Wood
+}
+
+public static class global
+{
+    public static string tilePath = "theme/tiles";
+    public static string backgroundPath = "theme/background";
+}
