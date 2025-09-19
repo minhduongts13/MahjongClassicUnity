aba5a4a1805e0dc8ace227ba74e71d7ebda28e11
minhduongts13 <quangminh4141@gmail.com>
2025-09-19 17:01:55 +0700

    languages popup and shop in progress

---
+++ Assets/components/ui/popup/languages/Languages.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Languages : BasePopup
+{
+    public GameObject content;
+    public void chooseLanguage(GameObject L)
+    {
+
+        var check = getCheck(L);
+        foreach (Transform child in content.transform)
+        {
+            if (child.gameObject != L)
+            {
+                var otherCheck = getCheck(child.gameObject);
+                otherCheck.SetActive(false);
+            }
+        }
+        check.SetActive(true);
+    }
+
+    public GameObject getCheck(GameObject L)
+    {
+        var checkbox = L.transform.GetChild(2);
+        return checkbox.transform.GetChild(0).gameObject;
+    }
+
+    
+}
