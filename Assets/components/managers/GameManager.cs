ce06c0ef140e4f2d10eb0a21229814eb7621d8d8
minhduongts13 <quangminh4141@gmail.com>
2025-09-23 10:44:57 +0700

    storage add on tool

--- Assets/components/managers/GameManager.cs
+++ Assets/components/managers/GameManager.cs
@@ -13,11 +13,11 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     [SerializeField] private GameObject sakurafail;
 
-public void showplay()
-{
-    sakurafail.SetActive(false);
-    UIManager.ShowPage(Page.PLAY);
-}
+    public void showplay()
+    {
+        sakurafail.SetActive(false);
+        UIManager.ShowPage(Page.PLAY);
+    }
 
     private int currentLevelNumber = 1;
     [SerializeField] public TilePool tilePool;
@@ -32,6 +32,9 @@ public void showplay()
     private Tile secondChosen;
     public bool highLight = true;
     public Stack<Tuple<Tuple<Vector3, Vector3>, Tuple<int, int>>> moves;
+
+    public GameObject hintButton;
+    public GameObject shuffleButton;
 
 
     void Start()
@@ -51,12 +54,13 @@ public void showplay()
     private void SetUp()
     {
         moves = new Stack<Tuple<Tuple<Vector3, Vector3>, Tuple<int, int>>>();
-       // currentLevelNumber = storageManager.getCurrentLevel();
+        // currentLevelNumber = storageManager.getCurrentLevel();
         currentLevel = LevelLoader.instance.GetLevel(currentLevelNumber);
         tilePool.SetUp();
         board.SetUp();
         matchManager.SetUp();
         pointManager.Setup();
+        setupTool();
         ShowMatchable();
     }
 
@@ -133,14 +137,14 @@ public void showplay()
         Debug.Log("reload");
         ShowMatchable();
     }
- 
+
     public void playSetUp()
     {
         this.SetUp();
     }
     public void showdash()
     {
-           sakurafail.SetActive(true);
+        sakurafail.SetActive(true);
         UIManager.ShowPage(Page.DASHBOARD);
         board.KillBoard();
     }
@@ -171,13 +175,34 @@ public void showplay()
         AdvanceLevel();
         Reload();
     }
-    
+
     public void ShowHint()
     {
         Tuple<Tile, Tile> hint = board.getHint();
         if (hint == null) return;
         hint.Item1.OnHint();
         hint.Item2.OnHint();
+
+        var numHints = storageManager.getNumberHints();
+        var bgNum0 = hintButton.transform.GetChild(2);
+        var bgNum1 = hintButton.transform.GetChild(0);
+        var textGO = hintButton.transform.GetChild(1).gameObject;
+        var text = textGO.GetComponent<TMPro.TextMeshProUGUI>();
+
+        if (numHints == 1)
+        {
+            bgNum0.gameObject.SetActive(true);
+            bgNum1.gameObject.SetActive(false);
+            textGO.SetActive(false);
+        }
+        else
+        {
+            bgNum0.gameObject.SetActive(false);
+            bgNum1.gameObject.SetActive(true);
+            text.text = (numHints - 1).ToString();
+            textGO.SetActive(true);
+        }
+        storageManager.setNumberHints(numHints - 1);
     }
     public async void Match()
     {
@@ -190,11 +215,74 @@ public void showplay()
     public async void Shuffle()
     {
         await board.Shuffle();
+        var numshuffles = storageManager.getNumberShuffles();
+        var bgNum0 = shuffleButton.transform.GetChild(2);
+        var bgNum1 = shuffleButton.transform.GetChild(0);
+        var textGO = shuffleButton.transform.GetChild(1).gameObject;
+        var text = textGO.GetComponent<TMPro.TextMeshProUGUI>();
+
+        if (numshuffles == 1)
+        {
+            bgNum0.gameObject.SetActive(true);
+            bgNum1.gameObject.SetActive(false);
+            textGO.SetActive(false);
+        }
+        else
+        {
+            bgNum0.gameObject.SetActive(false);
+            bgNum1.gameObject.SetActive(true);
+            text.text = (numshuffles - 1).ToString();
+            textGO.SetActive(true);
+        }
+        storageManager.setNumberShuffles(numshuffles - 1);
     }
     public void winDebug()
     {
         board.KillBoard();
         UIManager.ShowPopup(Popup.WIN);
+    }
+
+    public void setupTool()
+    {
+        var numHints = storageManager.getNumberHints();
+        var bgNum0 = hintButton.transform.GetChild(2);
+        var bgNum1 = hintButton.transform.GetChild(0);
+        var textGO = hintButton.transform.GetChild(1).gameObject;
+        var text = textGO.GetComponent<TMPro.TextMeshProUGUI>();
+
+        if (numHints == 0)
+        {
+            bgNum0.gameObject.SetActive(true);
+            bgNum1.gameObject.SetActive(false);
+            textGO.SetActive(false);
+        }
+        else
+        {
+            bgNum0.gameObject.SetActive(false);
+            bgNum1.gameObject.SetActive(true);
+            text.text = (numHints).ToString();
+            textGO.SetActive(true);
+        }
+
+        var numshuffles = storageManager.getNumberShuffles();
+        bgNum0 = shuffleButton.transform.GetChild(2);
+        bgNum1 = shuffleButton.transform.GetChild(0);
+        textGO = shuffleButton.transform.GetChild(1).gameObject;
+        text = textGO.GetComponent<TMPro.TextMeshProUGUI>();
+
+        if (numshuffles == 0)
+        {
+            bgNum0.gameObject.SetActive(true);
+            bgNum1.gameObject.SetActive(false);
+            textGO.SetActive(false);
+        }
+        else
+        {
+            bgNum0.gameObject.SetActive(false);
+            bgNum1.gameObject.SetActive(true);
+            text.text = (numshuffles).ToString();
+            textGO.SetActive(true);
+        }
     }
 
 }
--- Assets/components/ui/popup/shop/Shop.cs
+++ Assets/components/ui/popup/shop/Shop.cs
@@ -11,4 +11,16 @@ public class Shop : BasePopup
     {
         UIManager.HidePopup(Popup.SHOP);
     }
+
+    public void OnBuy1()
+    {
+        int hints = GameManager.instance.storageManager.getNumberHints();
+        hints += 1;
+        GameManager.instance.storageManager.setNumberHints(hints);
+    
+        int Shuffles = GameManager.instance.storageManager.getNumberShuffles();
+        Shuffles += 1;
+        GameManager.instance.storageManager.setNumberShuffles(Shuffles);
+
+    }
 }
